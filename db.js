04bd0cb1b22c4ad1d0089c4f357a71cb78fa773b
Dio Rovelino <63448336+floxydio@users.noreply.github.com>
2025-04-20 02:06:22 +0700

    .

--- db.js
+++ db.js
@@ -3,6 +3,11 @@ import { configDotenv } from 'dotenv'
 
 configDotenv()
 
+console.log(process.env.USER)
+console.log(process.env.DB_IP)
+console.log(process.env.PW)
+console.log(process.env.DB_NAME)
+
 const sql = postgres({
     host: process.env.DB_IP,
     port: 54320,
